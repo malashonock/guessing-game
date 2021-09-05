93a947f10fdfd1f25f8a1b887462e07ff3a82b4a
malashonock <malashonock@gmail.com>
2021-09-05 16:44:36 +0300

    feat: implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.range = [];
+        this.mid = null;
+        this.lastGuess = null;
+    }
 
     setRange(min, max) {
-
+        for (let i = min; i <= max; i++) {
+            this.range.push(i);
+        }
     }
 
     guess() {
-
+        this.mid = Math.floor(this.range.length / 2);
+        this.lastGuess = this.range[this.mid];
+        return this.lastGuess;
     }
 
     lower() {
-
+        this.range = this.range.splice(0, this.mid + 1);
     }
-
+    
     greater() {
-
+        this.range = this.range.splice(this.mid, this.range.length);
     }
 }
 
